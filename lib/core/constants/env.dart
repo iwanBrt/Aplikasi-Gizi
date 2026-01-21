c3a81ec97a334ee6fbbb837321ccf3f72e03f115
IwanBerutu12 <154679060+IwanBerutu12@users.noreply.github.com>
2026-01-21 09:17:47 +0700

    y

--- lib/core/constants/env.dart
+++ lib/core/constants/env.dart
@@ -5,5 +5,4 @@ class Env {
   static String get supabaseAnonKey => dotenv.env['SUPABASE_ANON_KEY'] ?? '';
   static String get geminiApiKey => dotenv.env['GEMINI_API_KEY'] ?? '';
   
-  //static const String openRouterApiKey = "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";
 }
